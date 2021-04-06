52b4a93720c8f8b0b9c2448879e36e3caa1b4b76
sakaiyoshiaki <yo4sio11.abc@gmail.com>
2021-04-06 13:22:01 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
